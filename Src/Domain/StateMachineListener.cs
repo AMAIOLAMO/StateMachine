eba44675a06a5b944d8990ddac8155643370b658
CXRedix <1135058679@qq.com>
2021-11-15 19:43:58 +0800

    Added Basic StateMachineListener

---
+++ Src/Domain/StateMachineListener.cs
@@ -0,0 +1,13 @@
+﻿namespace CXUtils
+{
+    public abstract class StateMachineListener<T> : IStateMachineListener<T>
+    {
+        public void Register( IStateMachine<T> stateMachine ) =>
+            stateMachine.StateChanged += StateChangedEventHandler;
+
+        public void UnRegister( IStateMachine<T> stateMachine ) =>
+            stateMachine.StateChanged -= StateChangedEventHandler;
+
+        protected abstract void StateChangedEventHandler( T previousState, T newState );
+    }
+}
