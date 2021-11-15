2ef58436e277f1e59208a6d722f54f38ea516d8b
CXRedix <1135058679@qq.com>
2021-11-15 17:02:18 +0800

    Added state machine listener

---
+++ Src/Domain/IStateMachineListener.cs
@@ -0,0 +1,11 @@
+﻿namespace CXUtils
+{
+    /// <summary>
+    ///     Implements a basic listener
+    /// </summary>
+    public interface IStateMachineListener<T>
+    {
+        void Register( IStateMachine<T> stateMachine );
+        void UnRegister( IStateMachine<T> stateMachine );
+    }
+}
